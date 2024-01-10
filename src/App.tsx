ac4c6f5757e9d1b0faf1dea0e5b4ea25aabcf498
NS-Sheam <123sheamfeni@gmail.com>
2024-01-10 23:46:36 +0600

    adding homework

--- src/App.tsx
+++ src/App.tsx
@@ -2,31 +2,34 @@ import { decrement, increment, incrementByValue } from "./redux/features/counter
 import { useAppDispatch, useAppSelector } from "./redux/hook";
 
 function App() {
-  const { count } = useAppSelector((state) => state.counter);
+  const { count, tally } = useAppSelector((state) => state.counter);
   const dispatch = useAppDispatch();
 
   return (
     <div className="h-screen w-full flex justify-center items-center">
-      <div className="flex gap-6 border-2 border-purple-300 p-10">
-        <button
-          onClick={() => dispatch(increment())}
-          className="px-3 py-2 rounded-md bg-green-500 text-xl font-semibold text-white"
-        >
-          Increment
-        </button>
-        <button
-          onClick={() => dispatch(incrementByValue(5))}
-          className="px-3 py-2 rounded-md bg-green-500 text-xl font-semibold text-white"
-        >
-          Increment by value
-        </button>
-        <h1 className="text-3xl">{count}</h1>
-        <button
-          onClick={() => dispatch(decrement())}
-          className="px-3 py-2 rounded-md bg-red-500 text-xl font-semibold text-white"
-        >
-          Decrement
-        </button>
+      <div className="border-2 border-purple-300 p-10 space-y-3">
+        <div className="flex gap-6 ">
+          <button
+            onClick={() => dispatch(increment())}
+            className="px-3 py-2 rounded-md bg-green-500 text-xl font-semibold text-white"
+          >
+            Increment
+          </button>
+          <button
+            onClick={() => dispatch(incrementByValue(5))}
+            className="px-3 py-2 rounded-md bg-green-500 text-xl font-semibold text-white"
+          >
+            Increment by value
+          </button>
+          <h1 className="text-3xl">{count}</h1>
+          <button
+            onClick={() => dispatch(decrement())}
+            className="px-3 py-2 rounded-md bg-red-500 text-xl font-semibold text-white"
+          >
+            Decrement
+          </button>
+        </div>
+        <p className="text-center font-bold text-xl">{tally}</p>
       </div>
     </div>
   );
--- src/redux/features/counterSlice.tsx
+++ src/redux/features/counterSlice.tsx
@@ -1,20 +1,32 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 type CounterType = {
   count: number;
+  tally: string;
 };
-const initialState: CounterType = { count: 0 };
+const initialState: CounterType = { count: 0, tally: "" };
 const counterSlice = createSlice({
   name: "counter",
   initialState,
   reducers: {
     increment: (state) => {
       state.count += 1;
+      state.tally = state.count % 5 === 0 ? state.tally + "/" : state.tally + ".";
     },
     decrement: (state) => {
       state.count -= 1;
+      state.tally = state.tally.slice(0, -1);
     },
     incrementByValue: (state, action: PayloadAction<number>) => {
       state.count += action.payload;
+      let tally = "";
+      if (state.count % 5 === 0) {
+        tally = state.tally + "/".repeat(action.payload / 5);
+      }
+      if (state.count % 5 !== 0) {
+        tally = state.tally + ".".repeat(action.payload);
+      }
+      state.tally = tally;
+      // state.tally = state.count % 5 !== 0 ? state.tally + ".".repeat(state.count % 5) : state.tally + "/";
     },
   },
 });
